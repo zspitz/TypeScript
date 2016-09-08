26b92ed465caf848c1386e95b38f974150292630
Andy Hanson <anhans@microsoft.com>
2016-09-08 09:19:33 -0700

    WIP

--- src/compiler/transformers/jsx.ts
+++ src/compiler/transformers/jsx.ts
@@ -157,12 +157,11 @@ namespace ts {
         }
 
         /**
-         * 
-         * 
          * JSX trims whitespace at the end and beginning of lines, except that the
          * start/end of a tag is considered a start/end of a line only if that line is
          * on the same line as the closing tag. See examples in
          * tests/cases/conformance/jsx/tsxReactEmitWhitespace.tsx
+         * See also https://www.w3.org/TR/html4/struct/text.html#h-9.1 and https://www.w3.org/TR/CSS2/text.html#white-space-model
          */
         function fixupWhitespaceAndDecodeEntities(text: string): string | undefined {
             let acc: string | undefined;
