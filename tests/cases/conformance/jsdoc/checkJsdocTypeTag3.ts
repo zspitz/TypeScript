33a4a54ec2a368bd0235ffd95772640034a7d936
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2017-06-01 15:55:23 -0700

    Update and add tests

---
+++ tests/cases/conformance/jsdoc/checkJsdocTypeTag3.ts
@@ -0,0 +1,31 @@
+﻿// @allowJS: true
+// @suppressOutputPathCheck: true
+
+// @filename: 0.js
+// @ts-check
+
+/** @type {object} */
+var obj1;
+obj1 = {};
+obj1 = 1;
+
+/** @type {Object} */
+var obj2;
+obj2 = {};
+obj2 = 1;
+
+/** @type {Object.<string, number>} */
+var obj3;
+
+/** @type number */
+var n1 = obj3["n1"];
+
+obj3 = {};
+
+/** @type {object.<string, number>} */
+var obj4;
+
+/** @type number */
+var n1 = obj4["n1"];
+
+obj4 = {};
---
+++ tests/cases/conformance/jsdoc/checkJsdocTypeTag4.ts
@@ -0,0 +1,23 @@
+﻿// @allowJS: true
+// @suppressOutputPathCheck: true
+
+// @filename: 0.js
+// @ts-check
+/** @type {Object.<string, number>} */
+var obj1;
+
+/** @type {string} */
+var n1 = obj1["n1"];
+
+/** @type {Object.<string, number>} */
+var obj2;
+obj2 = 1;
+
+/** @type {Object.<string>} */
+var obj3;
+
+/** @type {Object.<string, >} */
+var obj4;
+
+/** @type {Object.<number, string>} */
+var obj5;
